534f88b527377b471b4bc34b75227ab29776aa54
jpmlyr <jpmlyr@gmail.com>
2016-08-02 22:55:21 +0900

    junit로 변경

--- src/test/java/chap01/test/AccountTest.java
+++ src/test/java/chap01/test/AccountTest.java
@@ -1,15 +1,18 @@
 package chap01.test;
 
+import org.junit.Test;
+
 import chap01.main.Account;
 
 public class AccountTest {
 
+	/*
+	 * 아래의 테스트시나리오의 내용은
+	 * '계좌를 생성한다. 그때 특별한 에러는 없어야 한다' 이다.
+	 * */
+	@Test
 	public void testAccount() throws Exception {
 		Account account = new Account();
-
-		if (account == null) {
-			throw new Exception("계좌생성 실패");
-		}
 	}
 
 	public static void main(String[] args) {
