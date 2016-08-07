9ea042601ff70a515f3e95d7dabcf8ae320ce494
jpmlyr <jpmlyr@gmail.com>
2016-08-07 22:55:52 +0900

    .

--- src/test/java/chap01/test/AccountTest.java
+++ src/test/java/chap01/test/AccountTest.java
@@ -2,6 +2,7 @@ package chap01.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import chap01.main.Account;
@@ -17,16 +18,15 @@ public class AccountTest {
 	 */
 	@Test
 	public void testAccount() throws Exception {
-		setup();
 	}
 
-	private void setup() {
+	@Before
+	public void setup() {
 		account = new Account(10000);
 	}
 
 	@Test
 	public void testGetBalance() throws Exception {
-		setup();
 		assertEquals("10000원으로 계좌 생성 후 잔고 조회", 10000, account.getBalance());
 
 		account = new Account(1000);
@@ -38,15 +38,12 @@ public class AccountTest {
 	
 	@Test
 	public void testDeposit() throws Exception {
-		setup();
 		account.deposit(1000);
-
 		assertEquals(11000, account.getBalance());
 	}
 
 	@Test
 	public void testWithdraw() throws Exception {
-		setup();
 		account.withdraw(1000);
 		assertEquals(9000, account.getBalance());
 	}
